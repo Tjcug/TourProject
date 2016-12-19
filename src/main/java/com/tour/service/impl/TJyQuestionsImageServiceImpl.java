991411160da420335950fc02c405a31ee47e6fff
panpan <531826568@qq.com>
2016-12-19 15:29:03 +0800

    上传数据库sql文件，修改注释

--- src/main/java/com/tour/service/impl/TJyQuestionsImageServiceImpl.java
+++ src/main/java/com/tour/service/impl/TJyQuestionsImageServiceImpl.java
@@ -1,7 +1,9 @@
 package com.tour.service.impl;
 
+import com.tour.model.TJyquestions;
 import com.tour.model.TJyquestionsimage;
 import com.tour.service.TJyQuestionsImageService;
+import com.tour.service.TJyQuestionsService;
 import com.tour.util.JsonUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -56,14 +58,14 @@ public class TJyQuestionsImageServiceImpl extends BaseServiceImpl implements TJy
 	}
 
 	/**
-	 * 根据提问及应的id获取照片(第一张)
+	 * 根据提问及应的id获取照片
 	 * @param id 提问及应的id
 	 * @param type 0为取出第一张，1为取出所有照片
 	 * @return 返回json对象
 	 */
 	@Override
 	public List<TJyquestionsimage> getByQuestionID(long id,int type){
-		List<TJyquestionsimage> listJyquestionsimages = tJyquestionsimageDAO.findByProperty("TJyquestions",tJyquestionsDAO.getById(id));
+		List<TJyquestionsimage> listJyquestionsimages = tJyquestionsimageDAO.findByProperty("TJyquestions",tUserDAO.getById(id));
 		if(type == 0 && listJyquestionsimages.size()>0) {
 			List<TJyquestionsimage> jyquestionsimages = new ArrayList<>();
 			jyquestionsimages.add(listJyquestionsimages.get(0));
